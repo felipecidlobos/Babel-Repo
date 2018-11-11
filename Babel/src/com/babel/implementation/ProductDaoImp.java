50ef48874718271275bdd68dc944d300483549f8
Nicolás Partarrieu <phyc4s@gmail.com>
2018-11-11 14:37:23 -0300

    se agregan scripts sql, se arreglan detalles de implementacion del dao

--- Babel/src/com/babel/implementation/ProductDaoImp.java
+++ Babel/src/com/babel/implementation/ProductDaoImp.java
@@ -32,18 +32,64 @@ public class ProductDaoImp implements ProductDAO{
 	@Override
 	public void editProduct(Product product) {
 		// update a product
+		Session s = null;
+		
+		try {
+			s = HibernateUtil.sessionFactory.openSession();
+			s.beginTransaction();
+			s.update(product);
+			s.getTransaction().commit();
+		} catch(Exception ex){
+			System.out.println(ex.getMessage());
+		} finally {
+			if (s!=null) {
+				s.close();
+			}
+		}
 	}
 
 	@Override
 	public void deleteProduct(Product product) {
 		// delete a product
+		Session s = null;
 		
+		try {
+			s = HibernateUtil.sessionFactory.openSession();
+			s.beginTransaction();
+			s.delete(product);
+			s.getTransaction().commit();
+		} catch(Exception ex){
+			System.out.println(ex.getMessage());
+		} finally {
+			if (s!=null) {
+				s.close();
+			}
+		}
 	}
 
 	@Override
 	public List<Product> getProducts() {
 		// get a list with all the products
-		return null;
+		List<Product> listaGenial = null;
+		
+		Session s = HibernateUtil.sessionFactory.openSession();
+		Transaction t = s.beginTransaction();
+		String query = "FROM Product";
+		
+		try {
+			listaGenial = s.createQuery(query).getResultList();
+			t.commit();
+			s.close();
+		} catch(Exception ex){
+			System.out.println(ex.getMessage());
+			t.rollback();
+		} finally {
+			if (s!=null) {
+				s.close();
+			}
+		}
+		return listaGenial;
+		
 	}
 
 }
